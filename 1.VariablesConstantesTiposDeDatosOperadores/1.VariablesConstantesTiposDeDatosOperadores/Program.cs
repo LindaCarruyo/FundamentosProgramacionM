0e4b0b790be31959744e0ba4b6c5d9629f1a7be8
LindaCarruyo <linda.grajalesc.col@upb.edu.co>
2026-02-20 09:30:46 -0500

    subida de archivos+

---
+++ 1.VariablesConstantesTiposDeDatosOperadores/1.VariablesConstantesTiposDeDatosOperadores/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _1.VariablesConstantesTiposDeDatosOperadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //OPERADORES NUMERICOS
+            //Cambio de signo:
+            int dato1 = 4;
+            int dato2 = -dato1;
+            Console.Write(dato1 + " // " + dato2);
+
+            //Aritmeticos:
+            int dato3 = 4 + 6;
+            int dato4 = 150 - 120;
+            int dato5 = 12 * 56;
+            float dato6 = (float)5 / 3; //otras formas de dividir con decimales: [5/3f;] [5f/3;] [(float) 5/3;] [5f/3f;] )) se utilizó float para que muestre los decimales  
+            Console.Write(" \n Suma: {0}\n resta: {1}\n Multiplicación: {2}\n Divición: {3}\n", dato3, dato4, dato5, dato6);
+
+            //Incremento - Decremento}
+            dato3++; //dato3 = dato3+1;  solo 1
+            Console.WriteLine("Incremento: {0}", dato3);
+            dato4--; //dato4 = dato4-1;  solo 1
+            Console.WriteLine("Decremento: {0}", dato4);
+            dato5 += 4; // dato5 = dato5+4; //[para sumar ]
+            dato3 -= 5; // dato3 = dato3-5; //[para restar ]
+            dato5 *= dato3; // dato5 = dato5*4; //[para Multiplicar ]
+            dato6 /= dato3; // dato3 = dato3/5; //[para Dividir ]
+
+            //Orden de evaluación Operadores Numericos
+            float dato7 = 4 * 3 / 2;
+            Console.WriteLine(dato7);
+            float dato8 = 4.0f * (3.0f / 5.0f);
+            Console.WriteLine(dato8); 
+
+
+
+        }
+    }
+}
---
+++ 2.CondicionalesSimples-main/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.CondicionalesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // 1. crear un algritmo que permita... 
+
+            string nombre = "";
+            float sueldo = 0;
+
+            Console.WriteLine(" Ingrese el nombre del usuario: ");
+            nombre = Console.ReadLine(); //Todo lo que el usuario ingresa dese la consola llega como una cadena de caracteres (string)
+            Console.WriteLine(" Ingrese el sueldo del usuario: ");
+            sueldo = Convert.ToSingle(Console.ReadLine()); // Para convertir la cadena de caracteres a un numero decimal (float)
+             
+            if (sueldo >= 3000)
+            {
+                Console.WriteLine($"el usuario {nombre}, debe pagar impuestos");    // opción b) Console.WriteLine(" El usuario " + nombre + " tiene un sueldo alto");
+            }
+            if (sueldo < 3000)
+            {
+                Console.WriteLine($"el usuario {nombre}, NO debe pagar impuestos");
+            }
+            */
+
+            // 2.Crear un algoritmo que lea la edad del usuario, si el usuario es mayor a 18 años, se debe mostrar el mensaje: "bienvenido al sitio web "
+            float edad = 0;
+            Console.WriteLine("Ingrese la edad del usuario: ");
+            edad = Convert.ToSingle(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Bienvenido al sitio web >;D ");
+            }
+            if (edad < 18)
+            {
+                Console.WriteLine("Lo siento, no puedes ingresar al sitio web :c, vuelve cuando seas mayor de edad");
+            }
+            Console.WriteLine("hasta luego");
+        }
+    }
+}
